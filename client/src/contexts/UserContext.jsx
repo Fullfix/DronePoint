31d06f69cf8959d2ee6bdea5811acdd137dc1597
Nikita Breskanu <50447080+Fullfix@users.noreply.github.com>
2020-11-24 19:35:04 +0400

    create User Context

--- client/src/contexts/UserContext.jsx
+++ client/src/contexts/UserContext.jsx
@@ -1,16 +1,58 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
-const UserContext = createContext({
+export const UserContext = createContext({
     user: null,
     isAuthenticated: false,
-    token: null,
+    isLoading: true,
+    logout: () => {},
 })
 
-const UserProvider = () => {
+export default UserProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const token = localStorage.getItem('token');
+    axios.defaults.headers.common['Authorization'] = token;
+
+    const logout = () => {
+        localStorage.removeItem('Authorization');
+        setUser(null);
+    }
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const res = await axios.get('/auth/me');
+                setUser(res.data.response.user);
+            } catch (err) {
+                console.log(err);
+            }
+            setLoading(false);
+        }
+        if (loading) fetchUser();
+    }, [loading]);
+
+    if (loading) {
+        return (
+            <div>Loading...</div>
+        )
+    }
+    if (!user) {
+        return (
+            <Redirect to="/login"/>
+        )
+    }
+
     return (
-        <div>
-            
-        </div>
+        <UserContext.Provider value={{
+            user: user,
+            isAuthenticated: !!user,
+            isLoading: loading,
+            logout: logout,
+        }}>
+            {children}
+        </UserContext.Provider>
     )
 }
 
