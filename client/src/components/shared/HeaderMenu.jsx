08459a4a6df89d314e3c167019031af443dfd4d7
Nikita Breskanu <nbreskanu73@gmail.com>
2021-02-10 18:36:23 +0300

    Add isAuth showing

--- client/src/components/shared/HeaderMenu.jsx
+++ client/src/components/shared/HeaderMenu.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import { AppBar, Box, Toolbar, Typography } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { AppBar, Box, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { AccountCircle } from '@material-ui/icons';
+import { UserContext } from '../../contexts/UserContext';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        height: '64px',
+        [theme.breakpoints.down('sm')]: {
+            height: '56px',
+        }
+    },
+    img: {
+        height: '100%',
+        marginRight: '20px',
+    },
+    user: {
+        height: '44px',
+        width: '44px',
+        position: 'absolute',
+        right: 10,
+    }
+}))
 
 const HeaderMenu = ({ text }) => {
+    const classes = useStyles();
+    const { isAuthenticated } = useContext(UserContext);
     return (
         <AppBar position="sticky">
             <Toolbar>
-                <Box width="100%" height="100%" justifyContent="center" display="flex">
+                <Box display="flex" className={classes.root} alignItems="center">
+                    <img src={"/logo.svg"} className={classes.img}/>
                     <Typography variant="h2">{text}</Typography>
+                    {isAuthenticated && <AccountCircle className={classes.user}/>}
                 </Box>
             </Toolbar>
         </AppBar>
